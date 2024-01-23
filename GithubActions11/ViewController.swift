0ccb9c4ec397533f6695b9384e284dc54078a701
wael al mohammad <wail84n@gmail.com>
2024-01-23 20:45:15 +0300

    Initial Commit

---
+++ GithubActions11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubActions11
+//
+//  Created by wael on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubActions11UITests/GithubActions11UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GithubActions11UITestsLaunchTests.swift
+//  GithubActions11UITests
+//
+//  Created by wael on 1/23/24.
+//
+
+import XCTest
+
+class GithubActions11UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
